c6bade13b61e7ca1c4f8f8912c558a384a42a350
Gabriel Bueno <gabrielsbueno@hotmail.com>
2025-05-20 23:31:15 -0300

    initializing app

--- lambda/main.go
+++ lambda/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"lambda_func/app"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Username string `json:"username"`
-}
-
-// Take in a payload and do something with it
-func HandleRequest(event MyEvent) (string, error) {
-	if event.Username == "" {
-		return "", fmt.Errorf("username cannot be empty")
-	}
-
-	return fmt.Sprintf("Successfully called by - %s", event.Username), nil
-}
-
 func main() {
-	lambda.Start(HandleRequest)
+	myApp := app.NewApp()
+	lambda.Start(myApp.ApiHandler.RegisterUserHandler)
 }
