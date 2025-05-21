ec10821e2f8ca49a3bafd6cb512f5232600238e1
Gabriel Bueno <gabrielsbueno@hotmail.com>
2025-05-20 22:32:51 -0300

    setting up cdk and lambda

--- go_aws_project.go
+++ go_aws_project.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
@@ -18,12 +18,13 @@ func NewGoAwsProjectStack(scope constructs.Construct, id string, props *GoAwsPro
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	// The code that defines your stack goes here
+	lambdaProps := &awslambda.FunctionProps{
+		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
+		Handler: jsii.String("main"),
+		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
+	}
 
-	// example resource
-	// queue := awssqs.NewQueue(stack, jsii.String("GoAwsProjectQueue"), &awssqs.QueueProps{
-	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
-	// })
+	awslambda.NewFunction(stack, jsii.String("myLambdaFunction"), lambdaProps)
 
 	return stack
 }
---
+++ lambda/go.mod
@@ -0,0 +1,5 @@
+module lambda_func
+
+go 1.24.3
+
+require github.com/aws/aws-lambda-go v1.48.0
